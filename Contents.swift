3ca40f720fd4edbaf2203eb26816f155ae823bf1
Konstantin Popov <costachille@Konstantin.local>
2026-01-01 22:47:38 +0800

    Сделал

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+/*
+ Домашнее задание по теме "Функции".
+ 
+ Напишите функцию getTextSize,
+ которая принимает строку и возвращает длину этой строки в callback.
+ 
+ Для тестирования вашего решения, раскомментируйте блок внизу файла.
+ */
+func getTextSize(_ str: [String], completion: (Int) -> Void) {
+    let length = str.count
+    completion(length)
+}
+
+getTextSize("Привет!") { length in
+    print("Длина строки: \(length)")
+}
